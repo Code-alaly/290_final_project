45cff6da8dee1692429f912ded671f6800531024
code-alaly <dubiszd@gmail.com>
2020-05-25 14:41:21 -0700

    auto image scroll is now working

--- public/js/home.js
+++ public/js/home.js
@@ -1,39 +1,41 @@
 var slideIndex = 1;
 window.onload = function () {
     showSlides(slideIndex);
-    var intervalID = window.setInterval(auto_run, 3000);
+    //sets it to auto go to next slide every 3 seconds
+    window.setInterval(auto_run, 3000);
+
 }
 
 
 function plusSlides(n) {
+    // goes to next slide
     showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
+
     showSlides(slideIndex = n);
 }
 
 function auto_run() {
+    // does plus slide for one unit
     plusSlides(1)
 }
 
 function showSlides(n) {
+    // gets all slides, if it goes forward by 1, then it puts them all to not display except the last one
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var someOtherSlides = document.getElementsByClassName("otherSlides")
-    var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {
         slideIndex = 1
     }
     if (n < 1) {
         slideIndex = slides.length
     }
+    // all displays are none
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+    // except the last one
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
 }
